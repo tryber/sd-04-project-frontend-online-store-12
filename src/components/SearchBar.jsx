c2c44b6c2a608e161b8b7a9ad843c7b93b3336b4
Eder Sena <eder.sena@live.com>
2020-06-16 23:46:45 -0300

    Update SearchBar.jsx

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,14 +1,17 @@
-import React from 'react'
+import React from 'react';
 
 class SearchBar extends React.Component {
-    render() {
-      return (
-        <div className="field">
-            <input className="input is-rounded" type="text" />
-            <p data-testid='home-initial-message' className="home-initial-message subtitle is-4">Digite algum termo de pesquisa ou escolha uma categoria.</p>
-        </div>
-      )
-    }
+  render() {
+    return (
+      <div className="field">
+        <input className="input is-rounded" type="text" />
+        <p data-testid='home-initial-message'
+           className="home-initial-message subtitle is-4">
+           Digite algum termo de pesquisa ou escolha uma categoria.
+        </p>
+    </div>
+    )
   }
+}
 
 export default SearchBar
