e3b4d96b8818270e4487f1e62a463b4e995af20e
Rangel Paolo <paolorangel1996@outlook.com>
2020-06-17 16:29:01 -0300

    Adicionada componente SearchBar que contem uma barra de pesquisa e uma mensagem pasdrão.

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import * as api from './services/api';
 import './App.css';
+import SearchBar from './components/SearchBar';
 
 function App() {
   api.getCategories().then((response) => console.log(response));
   return (
     <div className="App">
       <Switch>
-        <Route exact path="/" />
+        <Route exact path="/" component={SearchBar} />
       </Switch>
     </div>
   );
