99307362565933faaf39a4bff14d33c915656f23
eder sena <eder.sena@live.com>
2020-06-18 18:47:56 -0300

    CC Categoria

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -6,32 +6,30 @@ import getCategories from '../services/api';
 
 class SearchBar extends Component {
   constructor() {
-    super()
-    this.state = {
-      category: []
-    }
-}
+    super();
+    this.state = { category: [], }
+};
 
-componentDidMount(){
-  getCategories().then(resp => this.setState({category: [resp]}));
-}
+  componentDidMount(){
+    getCategories().then((resp) => this.setState({ category: [resp] }));
+  };
 
   render() {
-      return (
+    return (
       <div className="container">
         <div className="alert alert-primary" role="alert">
-          sd-04-project-frontend-online-store-12  
+          sd-04-project-frontend-online-store-12
         </div>
         <div className="row">
           <div className="col-sm-2">
             <h4>Categorias:</h4>
             {this.state.category.length !== 0 ?
-              this.state.category[0].map((e) => <Category key={e.id} category={e.name}/>) :
+              this.state.category[0].map((e) => <Category key={ e.id } category={ e.name }/>) :
               null
             }
           </div>
           <div className="col-sm-8">
-            <input className="form-control" type="text"/>
+            <input className="form-control" type="text" />
             <p data-testid="home-initial-message" className="home-initial-message">
               Digite algum termo de pesquisa ou escolha uma categoria.
             </p>
