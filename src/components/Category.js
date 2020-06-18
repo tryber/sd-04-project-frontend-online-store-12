a1da2be7e91f46a13bf9f547c1a02314251f338e
Eder Sena <eder.sena@live.com>
2020-06-18 15:35:42 -0300

    Update Category.js

--- src/components/Category.js
+++ src/components/Category.js
@@ -6,27 +6,47 @@ class Category extends Component {
       <React.Fragment>
         <h4>Categorias:</h4>
         <div className="custom-control custom-checkbox">
-          <input type="checkbox" className="custom-control-input" id="defaultUnchecked" />
+          <input
+            type="checkbox"
+            className="custom-control-input"
+            id="defaultUnchecked"
+          />
           <label className="custom-control-label"
                  htmlFor="defaultUnchecked">Default unchecked</label>
         </div>
         <div className="custom-control custom-checkbox">
-          <input type="checkbox" className="custom-control-input" id="defaultUnchecked2" />
+          <input
+            type="checkbox"
+            className="custom-control-input"
+            id="defaultUnchecked2"
+          />
           <label className="custom-control-label"
                  htmlFor="defaultUnchecked2">Default unchecked</label>
         </div>
         <div className="custom-control custom-checkbox">
-          <input type="checkbox" className="custom-control-input" id="defaultUnchecked3" />
+          <input
+            type="checkbox"
+            className="custom-control-input"
+            id="defaultUnchecked3"
+          />
           <label className="custom-control-label"
                  htmlFor="defaultUnchecked3">Default unchecked</label>
         </div>
         <div className="custom-control custom-checkbox">
-          <input type="checkbox" className="custom-control-input" id="defaultUnchecked4" />
+          <input
+            type="checkbox"
+            className="custom-control-input"
+            id="defaultUnchecked4"
+          />
           <label className="custom-control-label"
                  htmlFor="defaultUnchecked4">Default unchecked</label>
         </div>
         <div className="custom-control custom-checkbox">
-          <input type="checkbox" className="custom-control-input" id="defaultUnchecked5" />
+          <input
+            type="checkbox"
+            className="custom-control-input"
+            id="defaultUnchecked5"
+          />
           <label className="custom-control-label"
                  htmlFor="defaultUnchecked5">Default unchecked</label>
         </div>
