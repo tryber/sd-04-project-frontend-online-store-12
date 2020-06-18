a31db1952bfd4638a47f2935283e835ff5b5c63d
Rangel Paolo <paolorangel1996@outlook.com>
2020-06-18 18:14:15 -0300

    Corrigido issue de importação redundante do pacote React-Router-Dom.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
-import { Switch, Route } from 'react-router-dom';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 // import * as api from './services/api';
 import './App.css';
 import SearchBar from './components/SearchBar';
