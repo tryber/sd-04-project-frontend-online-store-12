2069efe713c01850955a5a9850ced2c7a47459fb
Eder Sena <eder.sena@live.com>
2020-06-18 15:39:57 -0300

    Update Category.js

--- src/components/Category.js
+++ src/components/Category.js
@@ -1,9 +1,9 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 
 class Category extends Component {
   render() {
     return (
-      <React.Fragment>
+      <Fragment>
         <h4>Categorias:</h4>
         <div className="custom-control custom-checkbox">
           <input
@@ -12,45 +12,12 @@ class Category extends Component {
             id="defaultUnchecked"
           />
           <label className="custom-control-label"
-                 htmlFor="defaultUnchecked">Default unchecked</label>
+                 htmlFor="defaultUnchecked"
+          >
+                 Default unchecked
+          </label>
         </div>
-        <div className="custom-control custom-checkbox">
-          <input
-            type="checkbox"
-            className="custom-control-input"
-            id="defaultUnchecked2"
-          />
-          <label className="custom-control-label"
-                 htmlFor="defaultUnchecked2">Default unchecked</label>
-        </div>
-        <div className="custom-control custom-checkbox">
-          <input
-            type="checkbox"
-            className="custom-control-input"
-            id="defaultUnchecked3"
-          />
-          <label className="custom-control-label"
-                 htmlFor="defaultUnchecked3">Default unchecked</label>
-        </div>
-        <div className="custom-control custom-checkbox">
-          <input
-            type="checkbox"
-            className="custom-control-input"
-            id="defaultUnchecked4"
-          />
-          <label className="custom-control-label"
-                 htmlFor="defaultUnchecked4">Default unchecked</label>
-        </div>
-        <div className="custom-control custom-checkbox">
-          <input
-            type="checkbox"
-            className="custom-control-input"
-            id="defaultUnchecked5"
-          />
-          <label className="custom-control-label"
-                 htmlFor="defaultUnchecked5">Default unchecked</label>
-        </div>
-      </React.Fragment>
+      </Fragment>
     );
   }
 }
