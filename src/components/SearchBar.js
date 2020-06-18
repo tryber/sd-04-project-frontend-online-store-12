7dd3606a7aecd3c694835c2910810e43f0472317
Rangel Paolo <paolorangel1996@outlook.com>
2020-06-18 18:12:40 -0300

    Criado botão para fazer uso da API.

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -2,9 +2,22 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import CartIcon from './CartIcon';
 import Category from './Category';
+import Botao from './Botao';
+import * as api from '../services/api';
 
 class SearchBar extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { query: '' };
+    this.handleInput = this.handleInput.bind(this);
+  }
+
+  handleInput(e) {
+    this.setState({ query: e.target.value });
+  }
+
   render() {
+    const { query } = this.state;
     return (
       <div className="container">
         <div className="alert alert-primary" role="alert">
@@ -15,7 +28,18 @@ class SearchBar extends Component {
             <Category />
           </div>
           <div className="col-sm-8">
-            <input className="form-control" type="text" />
+            <input
+              data-testid="query-input"
+              className="form-control"
+              type="text"
+              value={query}
+              onChange={this.handleInput}
+            />
+            <Botao
+              texto="Buscar"
+              onClick={api.getProductsFromCategoryAndQuery}
+              queryValue={query}
+            />
             <p data-testid="home-initial-message" className="home-initial-message">
               Digite algum termo de pesquisa ou escolha uma categoria.
             </p>
