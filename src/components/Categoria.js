49944e0a7541d067d109b0404b7c34e56144b71d
Eder Sena <eder.sena@live.com>
2020-06-19 22:27:17 -0300

    update

--- src/components/Categoria.js
+++ src/components/Categoria.js
@@ -1,34 +1,36 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const Category = () => {
-  return (
-    <form action="#">
-      <p>
-        <input type="checkbox" id="test" />
-        <label htmlFor="test">Red</label>
-      </p>
-      <p>
-        <input type="checkbox" id="test1" />
-        <label htmlFor="test1">Red</label>
-      </p>
-      <p>
-        <input type="checkbox" id="test2" />
-        <label htmlFor="test2">Red</label>
-      </p>
-      <p>
-        <input type="checkbox" id="test3" />
-        <label htmlFor="test3">Red</label>
-      </p>
-      <p>
-        <input type="checkbox" id="test4" />
-        <label htmlFor="test4">Red</label>
-      </p>
-      <p>
-        <input type="checkbox" id="test5" />
-        <label htmlFor="test5">Red</label>
-      </p>
-    </form>
-  );
+class Category extends Component {
+  render() {
+    return (
+      <form action="#">
+        <p>
+          <input type="checkbox" id="test" />
+          <label htmlFor="test">Red</label>
+        </p>
+        <p>
+          <input type="checkbox" id="test1" />
+          <label htmlFor="test1">Red</label>
+        </p>
+        <p>
+          <input type="checkbox" id="test2" />
+          <label htmlFor="test2">Red</label>
+        </p>
+        <p>
+          <input type="checkbox" id="test3" />
+          <label htmlFor="test3">Red</label>
+        </p>
+        <p>
+          <input type="checkbox" id="test4" />
+          <label htmlFor="test4">Red</label>
+        </p>
+        <p>
+          <input type="checkbox" id="test5" />
+          <label htmlFor="test5">Red</label>
+        </p>
+      </form>
+    );
+  }
 }
 
 export default Category;
--- src/components/Produtos.js
+++ src/components/Produtos.js
@@ -1,18 +1,20 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const Produtos = () => {
-  return (
-    <form action="">
-      <div className="input-field">
-        <input
-          placeholder="Search movie"
-          id="first_name2"
-          type="text"
-          className="validate"
-        />
-      </div>
-    </form>
-  );
+class Produtos extends Component {
+  render() {
+    return (
+      <form action="">
+        <div className="input-field">
+          <input
+            placeholder="Search movie"
+            id="first_name2"
+            type="text"
+            className="validate"
+          />
+        </div>
+      </form>
+    );
+  }
 }
 
 export default Produtos;
