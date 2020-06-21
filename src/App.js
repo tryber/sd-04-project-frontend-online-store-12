9e43421746e3121b0c7524ee8d350b2a19969cf4
Eder Sena <eder.sena@live.com>
2020-06-21 14:54:37 -0300

    inputget

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Nav from './components/Nav';
 import Produtos from './components/Produtos';
@@ -6,39 +6,53 @@ import Categorias from './components/Categorias';
 import Carrinho from './components/Carrinho';
 import KartLInk from './components/KartLInk';
 
-function App() {
-  return (
-    <div>
-      <Router>
-        <Nav />
-        <div
-          className="row"
-          style={{ paddingTop: 30, paddingBottom: 30 }}
-        >
-          <div className="col s3">
-            <form action="#">
-              <Categorias />
-            </form>
+class App extends Component {
+  constructor(){
+    super()
+    this.state = {
+      searchTerm: '',
+    }
+  }
+
+  handleChange = (e) => {
+    this.setState({ searchTerm: e.target.value })
+  }
+
+  render() {
+    console.log(this.state.searchTerm)
+    return (
+      <div>
+        <Router>
+          <Nav />
+          <div
+            className="row"
+            style={{ paddingTop: 30, paddingBottom: 30 }}
+          >
+            <div className="col s3">
+              <form action="#">
+                <Categorias />
+              </form>
+            </div>
+            <Switch>
+              <Route exact path="/">
+                <div className="col s8">
+                  <Produtos handleChange={this.handleChange} />
+                </div>
+              </Route>
+              <Route exact path="/carrinho">
+                <div className="col s8">
+                  <Carrinho />
+                </div>
+              </Route>
+            </Switch>
+            <div className="col s1">
+              <KartLInk link="/carrinho" />
+            </div>
           </div>
-          <Switch>
-            <Route exact path="/">
-              <div className="col s7">
-                <Produtos />
-              </div>
-            </Route>
-            <Route exact path="/carrinho">
-              <div className="col s8">
-                <Carrinho />
-              </div>
-            </Route>
-          </Switch>
-          <div className="col s2">
-            <KartLInk link="/carrinho" />
-          </div>
-        </div>
-      </Router>
-    </div>
-  );
+        </Router>
+      </div>
+    );
+  };
 }
 
 export default App;
