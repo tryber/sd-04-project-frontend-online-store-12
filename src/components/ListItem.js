dd48ebb68a4dc0d9c42e15bc15193964031cb6fc
Rangel Paolo <paolorangel1996@outlook.com>
2020-06-21 18:43:37 -0300

    Exibindo imagem, título e preço de cada produto.

--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -1,8 +1,13 @@
 import React from 'react';
 
-const ListItem = ({ prod: { title } }) => (
+const ListItem = ({ prod: { title, thumbnail, price } }) => (
   <div data-testid="product">
     <p>{title}</p>
+    <img src={thumbnail} alt={title} />
+    <p>
+      Price: R$
+      {price.toFixed(2)}
+    </p>
   </div>
 );
 
--- src/components/Produtos.js
+++ src/components/Produtos.js
@@ -25,6 +25,7 @@ class Produtos extends Component {
 
   render() {
     const { searchTerm, produtos } = this.state;
+    console.log(produtos);
     return (
       <div className="col s8">
         <div className="input-field">
