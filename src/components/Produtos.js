4f14344727f0046ab2ba7998212a3f8b9cdf0cb9
Rangel Paolo <paolorangel1996@outlook.com>
2020-06-21 16:24:09 -0300

    Adiconados dat-testid no input text e button

--- src/components/Produtos.js
+++ src/components/Produtos.js
@@ -7,12 +7,14 @@ class Produtos extends Component {
       <div className="col s8">
         <div className="input-field">
           <input
+            data-testid="query-input"
             placeholder="Search movie"
             id="first_name2"
             type="text"
             className="validate"
             onChange={handleChange}
           />
+          <button data-testid="query-button" type="button">Buscar</button>
           <p data-testid="home-initial-message">
             Digite algum termo de pesquisa ou escolha uma categoria.
           </p>
