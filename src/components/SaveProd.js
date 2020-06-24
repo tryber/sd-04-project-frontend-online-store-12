c3b00d9e62416bf98618208650687926c02b84db
Rangel Paolo <paolorangel1996@outlook.com>
2020-06-23 22:33:06 -0300

    Refatoração 1 da SaveProd.

--- src/components/SaveProd.js
+++ src/components/SaveProd.js
@@ -3,6 +3,14 @@ import React from 'react';
 const SaveProd = (props) => {
   const { produto, test } = props;
 
+  const atualizaItem = (carrinho, newProd) => {
+    const noCarrinho = carrinho;
+    const existente = noCarrinho.find((item) => item.id === newProd.id);
+    const position = noCarrinho.indexOf(existente);
+    noCarrinho[position].quantidade += 1;
+    localStorage.shoppingKart = JSON.stringify(noCarrinho);
+  };
+
   const addToCart = (prod) => {
     const newProd = prod; // Evitando atribuição de parâmetros.
     if (Storage) {
@@ -11,12 +19,8 @@ const SaveProd = (props) => {
       const noCarrinho = JSON.parse(localStorage.shoppingKart);
       const existe = noCarrinho.some((item) => item.id === newProd.id);
 
-      console.log('São igual?', existe); // Implemntar o increment de quantidade.
       if (existe) {
-        const existente = noCarrinho.find((item) => item.id === newProd.id);
-        const position = noCarrinho.indexOf(existente);
-        noCarrinho[position].quantidade += 1;
-        localStorage.shoppingKart = JSON.stringify(noCarrinho);
+        atualizaItem(noCarrinho, newProd);
       } else {
         const newItems = [...noCarrinho, newProd];
         localStorage.shoppingKart = JSON.stringify(newItems);
